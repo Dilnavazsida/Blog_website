1e9677ba1d8ebba45357e18fa7486fee3e227c09
Dilnavazsida <navazdil07@gmail.com>
2025-06-13 18:52:27 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from . import models
+
+# Create your views here.
+@login_required(login_url='logIn')
+def index(request):
+    return render(request, 'home.html')
+
+def singIn(request):
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname =request.POST.get('lname')
+        username = request.POST.get('username') 
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        my_user = User.objects.create_user(username,email,password)
+        my_user.save()
+        return redirect('logIn') 
+    return render(request,'singin.html')
+
+def logIn(request):
+    if request.method == 'POST':
+        username = request.POST.get('username') 
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return redirect("logIn")
+    return render(request,'login.html')
+
+def logOut(request):
+    logout(request)
+    return redirect('singIn')
+
+def show_post(request):
+    posts = Post.objects.all()
+    return render(request,'show_post.html',{'posts':posts})
+
+def add_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        npost = models.Post(title=title,content=content,author = request.user)
+        npost.save()
+        return redirect('home')
+    return render(request,'add_post.html')
+
+
+def my_post(request):
+    context = {'posts':Post.objects.filter(author = request.user)}
+    return render(request,'mypost.html',context)
+
