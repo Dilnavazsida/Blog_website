72c9e715b4957b04bfb390774a1ab353a76be3b9
Dilnavazsida <navazdil07@gmail.com>
2025-06-29 23:10:56 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -130,4 +130,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'navazdil07@gmail.com'
-EMAIL_HOST_PASSWORD = "tgppocdsatzwkypq"
+EMAIL_HOST_PASSWORD = "add hear your password"
