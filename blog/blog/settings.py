bea64350932c341b863cc82e18ce91386d534ce4
Dilnavazsida <navazdil07@gmail.com>
2025-11-10 22:30:32 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -140,12 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #  for sending emails
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'navazdil07@gmail.com'
-# EMAIL_HOST_PASSWORD = ""
+
 
 LOGIN_URL = '/signin/'
 
