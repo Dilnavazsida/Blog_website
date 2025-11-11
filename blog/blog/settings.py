6524770bd0d6af25a5768363fb4f12d3245c2854
Dilnavazsida <navazdil07@gmail.com>
2025-11-11 11:59:27 +0530

    Collected static files for Render deployment

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #whitenoise helps Django serve static files (like admin CSS) automatically in production.
 ]
 
 ROOT_URLCONF = 'blog.urls'
